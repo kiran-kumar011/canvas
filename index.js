af108d08a79b9882a4f12ee578a128468bd3e63a
kiran-kumar011 <kirankirankumar56@gmail.com>
2019-06-28 00:11:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+ 		// bubble sorting.
+
+  		// sudo code. 
+  		// 1. iterate all elements from the array.
+  		// 2. based on index compare the stored 2 elements array which one is higher.	
+  		// 3. if index 0 value is higher than than index 1, then replace it.
+  		// 4. return the array in the main array.
+  		// 5. iterate for the next 2 elements. and repeat the same for the whole array length.
+  		// 6. the sorted value will always end at the last so remove it from the original array and shift it to the new sorted array.
+
+
+      // buble sorting method.
+  		// let arr = [8,7,6,5,4];
+
+  		// var temp;
+  		// let counter = 0;
+  		// function sorting(array) {
+  		// 	let isSorted = true;
+  		// 	for(j=0; j<array.length; j++) {
+	  	// 		for(i=0; i<array.length-j-1; i++) {
+	  	// 			if (array[i] > array[i+1]) {
+	  	// 				temp = array[i];
+	  	// 				array[i] = array[i+1];
+	  	// 				array[i+1] = temp;
+	  	// 				isSorted = false;
+	  	// 			}
+	  	// 		counter++;
+	  	// 		}
+  		// }
+  		// 	console.log(counter,array);
+	  	// 		if(isSorted) {
+	  	// 			return;
+	  	// 		}
+  		// }
